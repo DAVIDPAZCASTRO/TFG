5118a18f7718d441aaa74a39a2b25386a6ca9729
DAVIDPAZCASTRO <david.paz96@gmail.com>
2018-05-19 16:07:53 +0200

    Trivial files

---
+++ app/components/MCQuestionTrivial.jsx
@@ -0,0 +1,5 @@
+import * as Utils from '../vendors/Utils.js';
+import {objectiveAccomplished, objectiveAccomplishedThunk} from './../reducers/actions';
+
+import MCQuestionTrivialChoice from './MCQuestionTrivialChoice.jsx';
+import QuestionButtons from './QuestionButtons.jsx';
---
+++ app/components/Trivial.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../assets/scss/quiz.scss';
+
+import * as Utils from '../vendors/Utils.js';
+import {addObjectives, resetObjectives, finishApp} from './../reducers/actions';
+
+import Board from './Board.jsx';
+import Dice from './Dice.jsx';
+import MCQuestionTrivial from './MCQuestionTrivial.jsx';
+
+export default class Trivial extends React.Component {
+
+  render(){
+
+  }
+
+}
