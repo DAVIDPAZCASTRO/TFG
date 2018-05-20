6bbb8a7cab752533768dcc3c69f1828ce213907d
DAVIDPAZCASTRO <david.paz96@gmail.com>
2018-05-20 02:36:16 +0200

    trivial

--- app/components/Trivial.jsx
+++ app/components/Trivial.jsx
@@ -11,7 +11,12 @@ import MCQuestionTrivial from './MCQuestionTrivial.jsx';
 export default class Trivial extends React.Component {
 
   render(){
-
+    <div className="quiz">
+      <QuizHeader I18n={this.props.I18n} quiz={this.state.quiz} currentQuestionIndex={this.state.current_question_index}/>
+      <Board dispatch={this.props.dispatch} boxes={this.state.boxes} player={this.props.player} movement={this.props.movement} dice={this.props.dice} game_status={this.props.game_status}/>
+      <Dice dispatch={this.props.dispatch} dice={this.props.dice} game_status={this.props.game_status}/>
+      
+    </div>
   }
 
 }
